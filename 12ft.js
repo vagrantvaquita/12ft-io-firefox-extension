9cd8c84bac701090fd732d481373722c444d1ec5
Agustin RODRIGUEZ <a.rodriguez@naranja.com>
2024-07-10 13:04:13 -0300

    rename background script

--- 12ft.js
+++ 12ft.js
@@ -1,1 +1,29 @@
-document.body.style.border = "5px solid red";
+function onCreated(tab) {
+    console.log(`Created new tab: ${tab.id}`);
+  }
+  
+  function onError(error) {
+    console.log(`Error: ${error}`);
+  }
+
+function handleInstalled(details) {
+    browser.contextMenus.create(
+        {
+            id: "12ft",
+            title: "Open on 12ft.io",
+            contexts: ["link"],
+        }
+        );
+
+    browser.contextMenus.onClicked.addListener((info, tab) => {
+        if (info.menuItemId === "12ft") {
+            let creating = browser.tabs.create({
+            url: "https://12ft.io/" + info.linkUrl
+            });
+            creating.then(onCreated, onError);
+        }
+    });        
+}
+  
+
+browser.runtime.onInstalled.addListener(handleInstalled)
